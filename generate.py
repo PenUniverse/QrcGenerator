5efb48e3b3420537c1331844ac9cf77094a4e7f1
Redbeanw44602 <redbeana44945@gmail.com>
2022-10-24 12:08:13 +0800

    some fixes and improve.

--- generate.py
+++ generate.py
@@ -1,6 +1,9 @@
 import sys
-import xml
 import os
+
+if len(sys.argv) <= 1:
+    print('usage: generate.py <path>')
+    exit()
 
 path = sys.argv[1]
 output = 'custom.qrc'
@@ -9,14 +12,24 @@ result = '<RCC>\n'
 prefix_count = 0
 file_count = 0
 
+def std_dir(url:str):
+    url = url.replace('\\','/')
+    if url[:1] == '/':
+        url = url[1:]
+    if url[:1] == '/':
+        return std_dir(url)
+    return url
+
 for now,dir_list,file_list in os.walk(path):
     now = now[len(path):]
-    if now == '':
-        now = '/'
-    now = now.replace('\\','/')
-    result = result + '    <qresource prefix="%s">\n' % now
-    for n in file_list:
-        result = result + '        <file>%s%s</file>\n' % (now[1:],n)
+    now = std_dir(now)
+    result = result + '    <qresource prefix="/%s">\n' % now
+    for fn in file_list:
+        fn = std_dir(fn)
+        p = now
+        if p != '':
+            p = p + '/'
+        result = result + '        <file>%s%s</file>\n' % (p,fn)
         file_count += 1
     result = result + '    </qresource>\n'
     prefix_count += 1
